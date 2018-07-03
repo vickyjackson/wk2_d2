33079aefd8064c0ab830744170aa4739828f9b72
Vicky Jackson <vicky@digitalstuff.net>
2018-07-03 22:22:48 +0100

    Added a method to check if a river can breed fish and then breed them if true

--- river.rb
+++ river.rb
@@ -17,4 +17,27 @@ class River
     return @fish.length()
   end
 
+  def can_breed_fish()
+    fish_found = Hash.new(0)
+    for fish in @fish
+      fish_found[fish.name] += 1
+      if fish_found[fish.name] == 2
+        return true
+      else
+      end
+    end
+    return false
+  end
+
+  # Breed fish until total_fish_required has been met
+  def breed_fish(number_to_breed)
+    if (can_breed_fish() == true)
+      total_fish_required = @fish.length + number_to_breed
+      while (@fish.length < total_fish_required)
+        fish = Fish.new("Salmon")
+        @fish << fish
+        break if @fish.length == 100
+      end
+    end
+  end
 end
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -4,26 +4,41 @@ require_relative("../river")
 class TestRiver < Minitest::Test
 
   def setup()
-    @fish1 = Fish.new("One Fish")
-    @fish2 = Fish.new("Two Fish")
-    @fish3 = Fish.new("Red Fish")
-    @fish4 = Fish.new("Blue Fish")
-    @river = River.new("Esk", [@fish1, @fish2, @fish3, @fish4])
+    @fish1 = Fish.new("Salmon")
+    @fish2 = Fish.new("Clown")
+    @fish3 = Fish.new("Trout")
+    @fish4 = Fish.new("Cod")
+    @fish5 = Fish.new("Clown")
+    @river1 = River.new("Esk", [@fish1, @fish2, @fish3, @fish4, @fish5])
+    @river2 = River.new("Tay", [@fish1, @fish2, @fish3, @fish4])
   end
 
   # Test that the getter gets the correct name
   def test_river_name()
-    assert_equal("Esk", @river.name())
+    assert_equal("Esk", @river1.name())
   end
 
   # Test that number of fish in the river is 3
   def test_count_fish__3_fish()
-    actual = @river.count_fish()
-    assert_equal(4, actual)
+    actual = @river1.count_fish()
+    assert_equal(5, actual)
   end
 
-  # def test_breed_more_fish()
-  #
-  # end
+  def test_can_breed_fish()
+    actual = @river1.can_breed_fish()
+    assert_equal(true, actual)
+  end
+
+  def test_breed_fish__can_breed()
+    @river1.breed_fish(10)
+    actual = @river1.count_fish()
+    assert_equal(15, actual)
+  end
+
+  def test_breed_fish__cant_breed()
+    @river2.breed_fish(10)
+    actual = @river2.count_fish()
+    assert_equal(4, actual)
+  end
 
 end
